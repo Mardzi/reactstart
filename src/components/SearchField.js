35ec5e61d9d350a7472839a73d87bd60610cdbe7
mardzi <marko.milevski@outlook.com>
2022-07-11 01:41:53 +0200

    doesnt work

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -2,35 +2,62 @@ import './SearchField.css'
 import MockData from '../MockData.json'
 import { useState } from 'react';
 function SearchField(){
-    const [change, setChange] = useState(false);
     const [threeLetters, setThreeLetters] = useState('');
+    const [change, setChange] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-    const makeChange = (e) => {
-        setChange((e.prev = !e.prev));
-      };
+  const toggler = () => {
+    setToggle((prev) => !prev);
+  };
+
+  const makeChange = (e) => {
+    setChange((e.prev = !e.prev));
+  };
+
+
+    
 
     
        
     return (
         <div className='searchfield'>
             <form >
-                <input type="text" className='searchfieldinput' size='60' onKeyUp={makeChange} 
+                <input type="text" className='searchfieldinput' size='60'  onKeyUp={makeChange}
                 onChange={(event) => {
-                    threeLetters(event.target.value);
+                    setThreeLetters(event.target.value);
                   }}></input>
             </form>
-            <div className='container2'>
-                {MockData.filter((vrednost ) =>{
+            <div className='newclas'>
+                {MockData.filter((val ) =>{
                     if (threeLetters==''){
-                        return vrednost;
+                        return val;
                     }
                     else if(
-                        vrednost.first_name.toLowerCase().includes(threeLetters().toLowerCase())
+                        val.first_name.toLowerCase().includes(threeLetters().toLowerCase())
                     ){
-                        return vrednost;
+                        return val;
                     }
                     
-                }).map}
+                }).map((val, key) => {
+                    return (
+                        <div>
+                        <div className='newclas2' >
+                            <div  className='newclas'>
+                                <div>{ val.first_name} {val.last_name}</div>
+                            </div>
+                            <div className='newclas'>
+                                <div><img src= {val.avatar} alt={val.name} /></div>
+                                <div>{ val.first_name}</div>
+                                <div>{ val.last_name}</div>
+                                <div>{ val.email_address}</div>
+                                <div>{ val.profession}</div>
+                                <div>{ val.phone}</div>
+                                <div>{ val.rating }</div>
+                            </div>
+                        </div>
+                    </div>
+                    );
+                  })}
             </div>
         </div>
     );
