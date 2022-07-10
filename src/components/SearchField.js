99db098d8c673bd6e7fd3dba093a6cec5e8c2d49
mardzi <marko.milevski@outlook.com>
2022-07-11 01:04:10 +0200

    It works but it doesnt works :)

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,13 +1,37 @@
 import './SearchField.css'
-import ContactCard from './ContactCard';
+import MockData from '../MockData.json'
+import { useState } from 'react';
 function SearchField(){
-   
+    const [change, setChange] = useState(false);
+    const [threeLetters, setThreeLetters] = useState('');
+
+    const makeChange = (e) => {
+        setChange((e.prev = !e.prev));
+      };
+
+    
+       
     return (
         <div className='searchfield'>
             <form >
-                <input type="text" className='searchfieldinput' size='60'></input>
+                <input type="text" className='searchfieldinput' size='60' onKeyUp={makeChange} 
+                onChange={(event) => {
+                    threeLetters(event.target.value);
+                  }}></input>
             </form>
-            <div className='container2'><ContactCard /></div>
+            <div className='container2'>
+                {MockData.filter((vrednost ) =>{
+                    if (threeLetters==''){
+                        return vrednost;
+                    }
+                    else if(
+                        vrednost.first_name.toLowerCase().includes(threeLetters().toLowerCase())
+                    ){
+                        return vrednost;
+                    }
+                    
+                }).map}
+            </div>
         </div>
     );
     
